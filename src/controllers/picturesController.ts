d8fb6f5a04a75cfc4298991408890eefa6c85c6d
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-06-21 03:33:34 -0300

    feat: implement picture saving in directory with random name

--- src/controllers/picturesController.ts
+++ src/controllers/picturesController.ts
@@ -1,20 +1,28 @@
 import { Request, Response } from "express";
 import { exec } from "child_process";
+import shortid from "shortid";
 
 export default async function triggerPicture(req: Request, res: Response) {
-  const command = process.env.TAKE_PICTURE_COMMAND || "";
+  const pictureFilename = await takePicture();
 
-  const run = () => {
-    return new Promise((resolve, reject) => {
-      exec(command, (error, stdout, stderr) => {
-        if (error) return resolve(error);
-        if (stderr) return resolve(stderr);
-        resolve(stdout);
-      });
+  res.status(201).send(pictureFilename);
+}
+
+async function takePicture() {
+  const baseCommand = process.env.TAKE_PICTURE_BASE_COMMAND!;
+
+  const uid = shortid.generate();
+  const pictureFilename = `${uid}.jpg`;
+
+  const fullCommand = `${baseCommand} ${pictureFilename}`;
+
+  const result = await new Promise((resolve, reject) => {
+    exec(fullCommand, (error, stdout, stderr) => {
+      if (error) return resolve(error);
+      if (stderr) return resolve(stderr);
+      resolve(stdout);
     });
-  };
+  });
 
-  const result = await run();
-
-  res.status(201).send("Created");
+  return pictureFilename;
 }
