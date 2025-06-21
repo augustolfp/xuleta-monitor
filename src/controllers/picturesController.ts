8f61456f0c722399c4e797cc3f0022523b657a8f
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-06-21 03:11:19 -0300

    feat: implement image capture via http request

--- src/controllers/picturesController.ts
+++ src/controllers/picturesController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
+import { exec } from "child_process";
 
 export default async function triggerPicture(req: Request, res: Response) {
+  const command = process.env.TAKE_PICTURE_COMMAND || "";
+
+  const run = () => {
+    return new Promise((resolve, reject) => {
+      exec(command, (error, stdout, stderr) => {
+        if (error) return resolve(error);
+        if (stderr) return resolve(stderr);
+        resolve(stdout);
+      });
+    });
+  };
+
+  const result = await run();
+
   res.status(201).send("Created");
 }
