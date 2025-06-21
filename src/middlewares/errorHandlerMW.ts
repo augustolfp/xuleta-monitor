a461610d178f26aaf41a5e0a79d4e5e58263ef27
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-06-21 06:22:05 -0300

    feat: add error logs on discord webhooks

--- src/middlewares/errorHandlerMW.ts
+++ src/middlewares/errorHandlerMW.ts
@@ -1,13 +1,23 @@
 import { NextFunction, Request, Response } from "express";
 import { ApiError } from "../helpers/api-errors.js";
+import axios from "axios";
 
-export default function errorHandlerMW(
+export default async function errorHandlerMW(
   err: Error & Partial<ApiError>,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  console.log(err);
+  const payload = `\`\`\`json
+  ${JSON.stringify(err, null, 4)}
+  \`\`\``;
+
+  const discordErrorsWebhookUrl = process.env.DISCORD_ERRORS_WEBHOOK_URL!;
+
+  await axios.post(discordErrorsWebhookUrl, {
+    content: payload,
+  });
+
   const statusCode = err.statusCode ?? 500;
   const message = err.statusCode ? err.message : "Internal Server Error";
   res.status(statusCode).json({ message });
