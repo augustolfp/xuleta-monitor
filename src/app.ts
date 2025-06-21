46de5c5156a3e2379209d27afe195188fa73e43e
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-06-21 07:16:03 -0300

    feat: add cron job to trigger picture every 5 minutes

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,17 @@ import "express-async-errors";
 import express from "express";
 import cors from "cors";
 import errorHandlerMW from "./middlewares/errorHandlerMW.js";
+import cron from "node-cron";
+import axios from "axios";
 
 import router from "./routes/index.js";
 
 const app = express();
+
+cron.schedule("*/5 * * * *", () => {
+  //   console.log("running a task every minute");
+  axios.post("http://localhost:3000/picture");
+});
 
 app.use(express.json(), cors());
 app.use(router);
