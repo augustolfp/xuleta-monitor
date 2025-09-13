f01c52babadcb803d5181f2b517f5fd2aab0bf9b
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-09-13 13:34:34 -0300

    feat: show uptime every minute

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import errorHandlerMW from "./middlewares/errorHandlerMW.js";
 import cron from "node-cron";
 import axios from "axios";
+import os from "os";
 
 import router from "./routes/index.js";
 
@@ -12,6 +13,16 @@ const app = express();
 cron.schedule("*/5 * * * *", () => {
   //   console.log("running a task every minute");
   axios.post("http://localhost:3000/picture");
+});
+
+cron.schedule("* * * * *", () => {
+  const systemUptimeInSeconds = os.uptime();
+
+  const hours = Math.floor(systemUptimeInSeconds / 3600);
+  const minutes = Math.floor((systemUptimeInSeconds % 3600) / 60);
+  const seconds = Math.floor(systemUptimeInSeconds % 60);
+
+  console.log(`System uptime: ${hours} hours, ${minutes} minutes, ${seconds} seconds`);
 });
 
 app.use(express.json(), cors());
