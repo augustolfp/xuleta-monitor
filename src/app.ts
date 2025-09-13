85911b929463f9192609544207e4b601b8de4bee
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-09-13 13:41:34 -0300

    feat: implement status bot

--- src/app.ts
+++ src/app.ts
@@ -15,14 +15,25 @@ cron.schedule("*/5 * * * *", () => {
   axios.post("http://localhost:3000/picture");
 });
 
-cron.schedule("* * * * *", () => {
+cron.schedule("* * * * *", async () => {
   const systemUptimeInSeconds = os.uptime();
 
   const hours = Math.floor(systemUptimeInSeconds / 3600);
   const minutes = Math.floor((systemUptimeInSeconds % 3600) / 60);
   const seconds = Math.floor(systemUptimeInSeconds % 60);
 
-  console.log(`System uptime: ${hours} hours, ${minutes} minutes, ${seconds} seconds`);
+  const payload = `System uptime: ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+
+  // console.log(payload);
+
+  try {
+    const url = "https://discord.com/api/webhooks/1416458094073876662/_JtJiSNGnfbeS3AlGyu9iv1iCzIXrldFIjMfj3lWNS-NXmHLeWcSEnG1ErnD4ONHGkkA";
+    await axios.post(url, {
+      content: payload
+    });
+  } catch (error: any) {
+    console.log(error)
+  }
 });
 
 app.use(express.json(), cors());
